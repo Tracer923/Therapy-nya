033ab89d7b601cc361a2a30fef0529e1a78a16e4
Tracer923 <r.arisaka13@gmail.com>
2023-06-21 12:52:50 +0000

    rayout

--- config/routes.rb
+++ config/routes.rb
@@ -2,20 +2,23 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "homes#top"
   get "homes/about" => "homes#about", as: 'about'
-  
+
   resources :users
 
   resources :posts do
     resources :comments, only: [:create, :destroy]
     resources :likes, only: [:create, :destroy]
+    collection do
+      get 'confirm' # POST CONFIRMページのルートを追加
+    end
   end
 
-  #仮
+  # 仮
   resources :follows, only: [:create, :destroy]
   resources :notifications, only: [:index, :show]
   resources :reminders, except: [:index]
   
   resources :happy_memories, only: [:new, :create, :index, :show]
-  #ネガティブという文言はのちに変更
+  # ネガティブという文言はのちに変更
   resources :negative_memories, only: [:new, :create, :index, :show]
 end
