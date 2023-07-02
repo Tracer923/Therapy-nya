17d0c68aef733fefeb475447b3f2e31ad6c90a33
Tracer923 <r.arisaka13@gmail.com>
2023-07-02 13:05:16 +0000

    フォントをアップデートしました。

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,12 @@ module ApplicationHelper
     image = images.sample  # ランダムに画像を選択
     image_path(image)  # 画像のパスを返す
   end
+  
+  def random_message
+    messages = [
+    "猫は元来ミステリアスである。<br/>彼らの脳裏には、我々の認識を超えた数多のものが去来している。<br/><br/>Sir Walter Scott（ウォルター・スコット）",
+    "もし道に迷ったら、一番良い方法は猫について行くことだ。<br/>猫は道に迷わない。<br/><br/>Charles Monroe Schulz（チャールズ・M・シュルツ）",
+    "メッセージ3"] # ランダムなメッセージのリスト
+    messages.sample # ランダムにメッセージを選択して返す
+  end
 end
