a00485a95506f6869ca5b74e29e02444b7eeb3f5
Tracer923 <r.arisaka13@gmail.com>
2023-07-06 12:30:19 +0000

    add pics

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,10 @@
 module ApplicationHelper
   def random_image_path
-    images = ['nya1.jpg', 'nya2.jpg', 'nya3.jpg', 'nya4.jpg', 'nya5.jpg', 'nya6.jpg', 'nya7.jpg', 'nya8.jpg']  # 画像ファイル名のリスト
+    images = ['nya1.jpg', 'nya2.jpg', 'nya3.jpg', 'nya4.jpg', 'nya5.jpg', 'nya6.jpg', 'nya7.jpg', 'nya8.jpg', 'nya9.jpg', 'nya10.jpg', 'nya11.jpg']  # 画像ファイル名のリスト
     image = images.sample  # ランダムに画像を選択
     image_path(image)  # 画像のパスを返す
   end
-  
+
   def random_message
     messages = [
     "猫は元来ミステリアスである。<br/>彼らの脳裏には、<br>我々の認識を超えた数多のものが去来している。<br/><br/>Sir Walter Scott（ウォルター・スコット）",
